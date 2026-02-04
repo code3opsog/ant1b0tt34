20dada6b858e8b83a5dc6e3bdeb307f6603e95dc
code3opsog <119150557+code3opsog@users.noreply.github.com>
2026-02-03 19:06:41 -0500

    Update server.py

--- server.py
+++ server.py
@@ -351,4 +351,4 @@ if __name__ == '__main__':
     print("   pip install flask flask-cors requests")
     print("\n")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
